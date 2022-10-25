4dd3283dea03e83507218583932bf8c67f181c6e
itosubaru <103973953+itosubaru@users.noreply.github.com>
2022-10-25 10:27:20 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -1,2 +1,2 @@
 var who66 = '太郎'; function taro66(){alert(who66 + 'が来た!');}
-var who66-2 = '花子'; function hanako66(){alert(taro66-2 + 'が来た!');}
+var who66-2 = '花子'; function hanako66(){alert(twho66-2 + 'が来た!');}
