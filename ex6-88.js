058f9f367dea958ad72ce800d95774f8cf983f89
itosubaru <103973953+itosubaru@users.noreply.github.com>
2022-11-01 10:53:41 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,1 +1,1 @@
-function sayhello688(){document.getElmentById('who688').innerHTML='img src=./image/josairogoname.png';}
+function sayhello688(){document.getElmentById('who688').innerHTML='<img src=./image/josairogoname.png>';}
