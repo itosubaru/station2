9d354a88a249b106ae5091fa14fee2821bd0d6c0
itosubaru <103973953+itosubaru@users.noreply.github.com>
2022-11-08 14:14:44 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,1 +1,1 @@
-function rogochange1(){documentElementById('rogo1').innerHTML='<img src="./image/josairogoname.jpg" alt="城西ロゴ名前入り">';}
+function rogochange1(){document.getElementById('rogo1').innerHTML='<img src="./image/josairogoname.jpg" alt="城西ロゴ名前入り">';}
