af4fe01e37adf3c93557a4cad05f4eaf16d2d11d
itosubaru <103973953+itosubaru@users.noreply.github.com>
2022-11-13 16:59:22 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,2 +1,1 @@
-function imgchange1(){document.getElemntById('logo').src='./image/josairogo.png';
-function imgchange2(){document.getElmentById('logo').src='./image/josairogoname.jpg';
+function imgchange1(){ document.getElementById('logo').src='./image/josairogo.png';}
