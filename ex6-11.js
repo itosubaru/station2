289bd43b0c05a5214913dc2e7bb25e3de9ce0105
itosubaru <103973953+itosubaru@users.noreply.github.com>
2022-11-15 13:24:29 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -1,1 +1,1 @@
-function hyoji(){ //表示する文字 var str="一文字ずつ表示します。"; //テキストボックスの文字数 var cnt=document.timeer.moji.value.length; //文字が全部表示されているか確認
+function hyoji(){ var str="一文字ずつ表示します。"; var cnt=document.timeer.moji.value.length; 
