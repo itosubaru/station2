7d1a1f78dbd12a35c5dbd361828e74a990130c0c
itosubaru <103973953+itosubaru@users.noreply.github.com>
2022-11-22 10:39:30 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -1,1 +1,6 @@
-function hyoji(){ var startfnc="一文字ずつ表示します。"; var cnt=document.timer.moji.value.length;}
+function hyoji(){
+ //表示する文字 
+ var str="一文字ずつ表示します。";
+ //テキストボックスの文字数 
+ var cnt=document.timer.moji.value.length;}
+//文字が全部表示されているか確認
