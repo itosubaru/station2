7cd9313f771d3ea5d5f42f64a9691df55432fa60
itosubaru <103973953+itosubaru@users.noreply.github.com>
2022-12-13 10:19:29 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,4 +1,4 @@
 function imgchange3(){ document.getElementById('logo2').src='./image/josairogo.png';}
 
-function imgchange3(){ document.timer14.logo2="./image/josairogoname.jpg";}
+function hyoji14(){ document.timer14.logo2="./image/josairogoname.jpg";}
 function startfnc(){ setInterval("hyoji14()",1000);}
