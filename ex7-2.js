c95859b62020616ba39ab0c354a51ad7d0fb1ea7
itosubaru <103973953+itosubaru@users.noreply.github.com>
2022-12-26 14:13:01 +0900

    Update ex7-2.js

--- ex7-2.js
+++ ex7-2.js
@@ -1,4 +1,4 @@
 function add(){
-  var X = document.getElementById('input1').value;
-  var Y = document.getElementById('input2').value;
+  Number(var X = document.getElementById('input1').value);
+  Number(var Y = document.getElementById('input2').value);
   alert(X+Y);}
