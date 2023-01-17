4b56bdd28942d47098c4306f99ff8794238b8432
itosubaru <103973953+itosubaru@users.noreply.github.com>
2023-01-17 09:45:32 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,5 +1,5 @@
 function bmi(){
   var w = document.getElementById('input3').value;
   var h = document.getElementById('input4').value;
-  alert(number(w)/(number(h)**2));
+  alert(number(w)/(number(h)*number(h)));
 }
